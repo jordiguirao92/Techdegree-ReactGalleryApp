a5d2a8b4342ab697d354c0bf94b6e34b3a0285cc
jordiguirao92 <jordiguirao92@users.noreply.github.com>
2021-05-22 13:02:29 +0200

    Updates

---
+++ precache-manifest.c251809e492d1b0afd5362724406ee63.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "899f69ce4f95730e1af8d5871838db61",
+    "url": "/Techdegree-ReactGalleryApp/index.html"
+  },
+  {
+    "revision": "f8968f80764c40873088",
+    "url": "/Techdegree-ReactGalleryApp/static/css/main.28430a44.chunk.css"
+  },
+  {
+    "revision": "e0b6b0c471a02e5eed84",
+    "url": "/Techdegree-ReactGalleryApp/static/js/2.c9df2d90.chunk.js"
+  },
+  {
+    "revision": "78ddf5a465591a03b9601ba2ddd63415",
+    "url": "/Techdegree-ReactGalleryApp/static/js/2.c9df2d90.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f8968f80764c40873088",
+    "url": "/Techdegree-ReactGalleryApp/static/js/main.2d56763b.chunk.js"
+  },
+  {
+    "revision": "4a814d65e0826ce23158",
+    "url": "/Techdegree-ReactGalleryApp/static/js/runtime-main.e6df7071.js"
+  }
+]);
